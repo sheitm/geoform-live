7e84c8e42f1683e0b994e48e8b7d366a3037ddd3
Ståle Heitmann <ssheitmann@gmail.com>
2020-11-03 15:41:12 +0100

    Get event results on channel.

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/sheitm/ofever/scrape"
+import (
+	"github.com/sheitm/ofever/scrape"
+	"sync"
+)
 
 func main(){
-	scraper := scrape.NewScraper()
-	_ = scraper
+
+	eventChan := make(chan *scrape.Result)
+
+	scrape.StartScrape("https://ilgeoform.no/rankinglop/res2020-10-03.html", eventChan)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	go func(ec <-chan *scrape.Result, wg *sync.WaitGroup) {
+		r := <- ec
+		e := r.Event
+		_ = e
+		wg.Done()
+	}(eventChan, wg)
+
+	wg.Wait()
 }
--- scrape/scrape.go
+++ scrape/scrape.go
@@ -1,11 +1,33 @@
 package scrape
 
-import "github.com/sheitm/ofever/contracts"
+import (
+	"github.com/sheitm/ofever/contracts"
+	"net/http"
+)
 
-type Scraper interface {
+type scraper interface {
 	Scrape(url string) (*contracts.Event, error)
 }
 
-func NewScraper() Scraper {
-	return &eventScraper{}
+// Result is the result of a single event scraping session.
+type Result struct {
+	// URL is the provided address to the page containing the results for the event.
+	URL   string
+
+	// Event is the details of the event.
+	Event *contracts.Event
+
+	// Error details if any error occurred during execution.
+	Error error
+}
+
+func StartScrape(url string, resultChan chan<- *Result) {
+	go func(url string, resultChan chan<- *Result) {
+		res := &Result{URL: url}
+		scraper := &eventScraper{client: &http.Client{}}
+		event, err := scraper.Scrape(url)
+		res.Event = event
+		res.Error = err
+		resultChan <- res
+	}(url, resultChan)
 }
